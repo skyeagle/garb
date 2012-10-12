b7f255c8e9671d6d030c1cbb4023819a15c6a2e9
Anton Orel <eagle.anton@gmail.com>
2012-10-12 16:03:07 +0400

    Mongoid 2.4 fixes

--- lib/garb/core_ext/symbol.rb
+++ lib/garb/core_ext/symbol.rb
@@ -7,32 +7,42 @@ module SymbolOperatorMethods
     :lt      => '<',
     :lte     => '<=',
     :matches => '==',
-    
+    :elemMatch => '==', # for Mongoid 2.4
+
     :does_not_match   => '!=',
     :contains         => '=~',
     :does_not_contain => '!~',
     :substring        => '=@',
     :not_substring    => '!@',
-    
+
     :desc       => '-',
     :descending => '-'
   }
   SLUGS = OPERATORS.keys.freeze
 
   def to_google_analytics
-    t = Garb.to_google_analytics @field || @target
-    o = OPERATORS[@operator]
+    t = Garb.to_google_analytics(@field || @target || @key)
+    o = OPERATORS.with_indifferent_access[@operator]
 
+    [:desc, :descending].include?(@operator.to_sym) ? "#{o}#{t}" : "#{t}#{o}"
     [:desc, :descending].include?(@operator.to_sym) ? "#{o}#{t}" : "#{t}#{o}"
   end
 end
 
 class SymbolOperator
   include SymbolOperatorMethods
-  
+
   def initialize(field, operator)
     @field, @operator = field, operator
   end unless method_defined? :initialize
+end
+
+if Object.const_defined?("Mongoid")
+  require 'mongoid/criterion/complex'
+
+  class Mongoid::Criterion::Complex
+    include SymbolOperatorMethods
+  end
 end
 
 symbol_slugs = if Object.const_defined?('DataMapper')
@@ -49,6 +59,14 @@ else
   SymbolOperatorMethods::SLUGS
 end
 
+# define the remaining symbol operators
+symbol_slugs.each do |operator|
+  Symbol.class_eval <<-RUBY
+    def #{operator}
+      SymbolOperator.new self, :#{operator}
+    end unless method_defined? :#{operator}
+  RUBY
+end
 # define the remaining symbol operators
 symbol_slugs.each do |operator|
   Symbol.class_eval <<-RUBY
